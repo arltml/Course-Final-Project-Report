330dc8f71a4a775b1f9a0bfd34645b0aaca4de02
arltml <amarti10@stevens.edu>
2026-05-05 22:46:01 -0400

    additional methods added:  retrieve number of data, predict wine class, and get stats

--- models/wine_dataset.py
+++ models/wine_dataset.py
@@ -27,3 +27,42 @@ class WineDataset:
             label = row["target"]
             sample = WineSample(features, label)
             self.samples.append(sample)
+    def __len__(self):
+        """
+        return the number of samples within the dataset as an integer
+        """
+        return len(self.samples)
+    
+    def train_model(self):
+        """
+        Uses and trains KNN classifier using the provided database.
+        """
+        if self.df is None:
+            raise ValueError("Dataset not loaded.")
+
+        X = self.df.drop("target", axis=1)
+        y = self.df["target"]
+
+        self.model = KNeighborsClassifier(n_neighbors=3)
+        self.model.fit(X, y)
+    
+    def predict(self, sample:WineSample):
+        """
+        Predict the class of a wine sample
+        """
+        if self.model is None:
+            raise ValueError("Model not trained.")
+        
+        feature_vector = [list(sample.features.values())]
+        return self.model.predict(feature_vector)[0]
+    
+    def get_summary_stats(self):
+        """
+        Compute summary statistics for dataset.
+        """
+        if self.df is None:
+            raise ValueError("Dataset not loaded.")
+        return{
+            col: self.df[col].mean()
+            for col in self.df.columns if col != "target"
+        }
